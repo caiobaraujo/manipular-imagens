a1664a301920fd47568dc891dd276e2f14c76e22
caiobaraujo <caiobrega@gmail.com>
2021-12-21 19:55:30 -0300

    projeto pronto

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 var photoFile = document.getElementById('photo-file');
+let photoPreview = document.getElementById('photo-preview');
+let image;
+let photoName;
 
 document.getElementById('select-image').onclick = function () {
   photoFile.click();
@@ -7,13 +10,153 @@ document.getElementById('select-image').onclick = function () {
 window.addEventListener('load', () => {
   photoFile.addEventListener('change', () => {
     let file = photoFile.files[0];
+    photoName = file.name;
 
     let reader = new FileReader();
     reader.readAsDataURL(file);
 
     reader.onload = function (event) {
-      let image = document.getElementById('photo-preview');
+      image = new Image();
       image.src = event.target.result;
+      image.onload = onLoadImage;
     };
   });
 });
+
+//selection toll
+const selection = document.getElementById('selection-tool');
+let startSelection = false;
+let startX,
+  startY,
+  relativeStartX,
+  relativeStartY,
+  endX,
+  endY,
+  relativeEndX,
+  relativeEndY;
+
+const events = {
+  // O primeiro esta escrito como "chave-valor", só para visualizacao
+  mouseover: function () {
+    this.style.cursor = 'crosshair';
+  },
+  mousedown() {
+    const { clientX, clientY, offsetX, offsetY } = event;
+    /*  console.table({
+      client: { clientX, clientY },
+      offset: { offsetX, offsetY },
+    }); */
+
+    startX = clientX;
+    startY = clientY;
+    relativeStartX = offsetX;
+    relativeStartY = offsetY;
+
+    startSelection = true;
+  },
+  mousemove() {
+    endX = event.clientX;
+    endY = event.clientY;
+
+    if (startSelection) {
+      selection.style.display = 'initial';
+      selection.style.top = startY + 'px';
+      selection.style.left = startX + 'px';
+
+      selection.style.width = endX - startX + 'px';
+      selection.style.height = endY - startY + 'px';
+    }
+  },
+  mouseup() {
+    startSelection = false;
+    //selection.style.display = 'none';
+
+    relativeEndX = event.layerX;
+    relativeEndY = event.layerY;
+
+    cropButton.style.display = 'initial';
+  },
+};
+
+Object.keys(events).forEach((eventName) => {
+  photoPreview.addEventListener(eventName, events[eventName]);
+});
+
+//Canvas
+
+let canvas = document.createElement('canvas');
+let ctx = canvas.getContext('2d');
+
+function onLoadImage() {
+  const { width, height } = image;
+  canvas.width = width;
+  canvas.height = height;
+
+  // limpar o contexto
+  ctx.clearRect(0, 0, width, height);
+
+  // desenhar a imagem no contexto
+  ctx.drawImage(image, 0, 0);
+
+  photoPreview.src = canvas.toDataURL();
+}
+
+//recortar imagem
+const cropButton = document.getElementById('crop-image');
+
+cropButton.onclick = function () {
+  const { width: imgW, height: imgH } = image;
+  const { width: previewW, height: previewH } = photoPreview;
+
+  const [widthFactor, heightFactor] = [+(imgW / previewW), +(imgH / previewH)];
+
+  const [selectionWidth, selectionHeight] = [
+    +selection.style.width.replace('px', ''),
+    +selection.style.height.replace('px', ''),
+  ];
+
+  const [croppedWidth, croppedHeight] = [
+    +(selectionWidth * widthFactor),
+    +(selectionHeight * heightFactor),
+  ];
+
+  const [actualX, actualY] = [
+    +(relativeStartX * widthFactor),
+    +(relativeStartY * heightFactor),
+  ];
+
+  // pegar do ctx a imagem cortada
+  const croppedImage = ctx.getImageData(
+    actualX,
+    actualY,
+    croppedWidth,
+    croppedHeight
+  );
+
+  // limpar o ctx
+  ctx.clearRect(0, 0, ctx.width, ctx.height);
+
+  // ajuste de proporções
+  image.width = canvas.width = croppedWidth;
+  image.height = canvas.height = croppedHeight;
+
+  // adicionar a imagem cortada ao ctx
+  ctx.putImageData(croppedImage, 0, 0);
+
+  // esconder a ferramenta de seleção
+  selection.style.display = 'none';
+
+  // atualizar o preview da imagem
+  photoPreview.src = canvas.toDataURL();
+
+  // mostrar o botão de download
+  downloadButton.style.display = 'initial';
+};
+
+const downloadButton = document.getElementById('download');
+downloadButton.onclick = function () {
+  const a = document.createElement('a');
+  a.download = photoName + '-cropped.png';
+  a.href = canvas.toDataURL();
+  a.click();
+};
